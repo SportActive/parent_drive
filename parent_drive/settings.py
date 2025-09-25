4eeba5774f56ee81da6e7fa5d1cf05c0360ca79e
Kyrylo <kirill.grig.2000@gmail.com>
2025-09-25 21:56:21 +0300

    Force complete rebuild

--- parent_drive/settings.py
+++ parent_drive/settings.py
@@ -1,19 +1,21 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key' # Your key will be different
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-local-dev')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parentdrive.up.railway.app', '.railway.app', 'localhost', '127.0.0.1']
 
 
-# Application definition
+# --- НОВІ НАЛАШТУВАННЯ ДЛЯ РОБОТИ ЗА ПРОКСІ ---
+# Ці рядки кажуть Django довіряти заголовкам, які надсилає Railway
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ----------------------------------------------
+
 
 INSTALLED_APPS = [
     'scheduler.apps.SchedulerConfig',
@@ -27,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -42,21 +45,12 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+        'OPTIONS': { 'context_processors': [ 'django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', ], },
     },
 ]
 
 WSGI_APPLICATION = 'parent_drive.wsgi.application'
 
-
-# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -64,32 +58,19 @@ DATABASES = {
     }
 }
 
+AUTH_PASSWORD_VALIDATORS = []
 
-# --- THE FIX IS HERE ---
-# We are commenting out the password validators to disable them.
-# AUTH_PASSWORD_VALIDATORS = [
-#     { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-#     { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-#     { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-#     { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-# ]
-
-
-# Internationalization
 LANGUAGE_CODE = 'uk'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Redirect settings
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
