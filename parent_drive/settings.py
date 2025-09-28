7c821cef0e43699a586ff12ac823d8f6363bfdc9
Kyrylo <kirill.grig.2000@gmail.com>
2025-09-28 17:10:17 +0300

    Revert temporary DB settings after manual migrate

--- parent_drive/settings.py
+++ parent_drive/settings.py
@@ -54,11 +54,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'parent_drive.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+#   'default': dj_database_url.config(
+#      default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#     conn_max_age=600
+#)
+
+    'default': dj_database_url.parse('postgresql://postgres:gbThqFnCdAydFzFrAUUpPxPwTAOeVihP@postgres.railway.internal:5432/railway')
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
