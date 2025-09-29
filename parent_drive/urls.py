e0169d3a0031d5f1510c53ab39c05a1563e1fc44
Kyrylo <kirill.grig.2000@gmail.com>
2025-09-30 01:03:15 +0300

    Use PG environment variables

--- parent_drive/urls.py
+++ parent_drive/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from scheduler import views as scheduler_views  # <-- Ось рядок, який потрібно додати
 
 urlpatterns = [
     path('admin/', admin.site.urls),
